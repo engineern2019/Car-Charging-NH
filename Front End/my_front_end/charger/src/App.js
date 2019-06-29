fc0a49ba6364129facda88bcd43d9c265d7bd719
engineern2019 <alidawood15@gmail.com>
2019-06-29 14:53:05 +0100

    Cors added to the app.js

--- Front End/my_front_end/charger/src/App.js
+++ Front End/my_front_end/charger/src/App.js
@@ -2,25 +2,32 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  construction(prop) {
+    super(prop);
+    this.state = { apiResponse: ""};
+  }
+  callAPI() {
+    fetch("http://localhost:9000/testAPI")
+          .then(res => res.text())
+          .then(res => this.setState({ apiResponse: res}))
+          .catch(err => err);
+  }
+
+  componentDidMount() {
+    this.callAPI();
+  }
+  render() {
+    return(
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-header" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-title">Welcome to React</p>
+      </div>
+    )
+  }
 }
 
 export default App;
